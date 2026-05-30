38416e599f64341b2135392cfc25569b029a8fb6
Mohit <mohitjaiswal.bltr@gmail.com>
2026-05-31 00:48:10 +0530

    Dashboard changes

---
+++ src/app/customer/customer.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-customer',
+  templateUrl: './customer.component.html',
+  styleUrls: ['./customer.component.scss']
+})
+export class CustomerComponent {}
